0818f2e7ce74427653dbf86325caccc0ddaca6d7
DongZhouGu <gdz678@163.com>
2021-11-06 18:24:11 +0800

    fix

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def report_day(sess, t, user, config):
 
     ShiFSH, ShiFZX, ddlSheng, ddlShi, ddlXian, XiangXDZ, ShiFZJ = get_last_report(sess, t)
 
-    SuiSM, XingCM = get_img_value(sess)
+    XingCM = get_img_value(sess)
     # ShiFSH, ShiFZX, ddlSheng, ddlShi, ddlXian, XiangXDZ = get_last_report(sess, t)
     print(f'是否在上海：{ShiFSH}', f'是否在校：{ShiFZX}', ddlSheng, ddlShi, ddlXian, '详细地址已隐去')
 
@@ -87,9 +87,11 @@ def report_day(sess, t, user, config):
                 "__VIEWSTATE": view_state['value'],
                 "__VIEWSTATEGENERATOR": "7AD7E509",
                 "p1$ChengNuo": "p1_ChengNuo",
+                "p1$pnlDangSZS$DangSZS": "A",
                 "p1$BaoSRQ": BaoSRQ,
                 "p1$DangQSTZK": "良好",
                 "p1$TiWen": "",
+                "p1$pImages$HFimgXingCM": XingCM,
                 "p1$JiuYe_ShouJHM": "",
                 "p1$JiuYe_Email": "",
                 "p1$JiuYe_Wechat": "",
@@ -133,15 +135,14 @@ def report_day(sess, t, user, config):
                 "p1$SuiSM": "绿色",
                 "p1$LvMa14Days": "是",
                 "p1$Address2": "",
-                "F_TARGET": "p1_ctl00_btnSubmit",
+                "p1_pnlDangSZS_Collapsed": "false",
+                "p1_pImages_Collapsed": "false",
                 "p1_ContentPanel1_Collapsed": "true",
                 "p1_GeLSM_Collapsed": "false",
                 "p1_Collapsed": "false",
-                "p1$pImages$HFimgSuiSM": SuiSM,
-                "p1$pImages$HFimgXingCM": XingCM,
                 "F_STATE": generate_fstate_day(BaoSRQ, ShiFSH, ShiFZX,
                                                ddlSheng, ddlShi, ddlXian, XiangXDZ, ShiFZJ,
-                                               SuiSM, XingCM)
+                                               XingCM)
             }, headers={
                 'X-Requested-With': 'XMLHttpRequest',
                 'X-FineUI-Ajax': 'true'
